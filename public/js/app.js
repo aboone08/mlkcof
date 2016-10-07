9df40afa10e539d0470efd7bacb92d2f170b9836
aboone08 <andranique.boone@gmail.com>
2016-10-07 17:25:28 -0400

    begin work on the about section, adding parallax and board of directors

---
+++ public/js/app.js
@@ -0,0 +1,4 @@
+
+  $('.parallax-window').parallax({imageSrc: 'css/images/marchbanner.jpg'});
+  $('.parallax-window2').parallax({imageSrc: 'css/images/director.jpg'});
+  $('.parallax-window3').parallax({imageSrc: 'images/contact.jpg'});
